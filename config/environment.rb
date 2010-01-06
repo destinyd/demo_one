c6cef1e38b87215bd4089598b017702bd13e18b4
destinyd <destinyd_war@163.com>
2010-01-06 16:24:22 +0800

    i18n test

--- config/environment.rb
+++ config/environment.rb
@@ -70,6 +70,6 @@ Rails::Initializer.run do |config|
   # Activate observers that should always be running
   # config.active_record.observers = :cacher, :garbage_collector
   config.i18n.default_locale = "zh-CN"
+  config.i18n.load_path += Dir[File.expand_path(File.dirname(__FILE__) + '/locale/*.{rb,yml}')]
 end
-  I18n.load_path += Dir[File.expand_path(File.dirname(__FILE__) + '/../locale/*.{rb,yml}')]
 
